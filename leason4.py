e580cfa97ed319ecbc581f421a11fc5af45d9718
luckyamina <144383353+luckyamina@users.noreply.github.com>
2023-11-03 19:32:35 +0400

    Add files via upload

---
+++ leason4.py
@@ -0,0 +1,15 @@
+#Users Age and Calculation 
+age = int(input("Enter your age: "))
+print(age)
+age = age * 7 / 3
+print(age)
+sum = age + (4 * 9)
+print(sum)
+
+#Car Price
+Old_price = 7000
+New_price = Old_price + Old_price * 0.62
+Price_increment = New_price - Old_price
+print("Old price was: " + str(Old_price) + "$. New price is: " + str(New_price) + "$. Price increased by: " + str(Price_increment)+ "$")
+
+
